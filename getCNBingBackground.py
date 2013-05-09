704474d77fc2a0766b44bbfea3439444f416ef4e
Vizhen <ohtianla@gmail.com>
2013-05-09 09:46:41 +0800

    完善格式

--- getCNBingBackground.py
+++ getCNBingBackground.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
-
+# coding=utf-8
 import urllib
 
 def readCNBingPage():
     return urllib.urlopen("http://cn.bing.com").read()
     
-def getImageUrl():
-    content = readCNBingPage()
+def getImageUrl(content):
     tagpos = content.find("g_img=")
     if tagpos <> -1:
       imageStart = content.find("'", tagpos)
       imageEnd = content.find("',", tagpos)
       return content[imageStart + 1:imageEnd]
     if tagpos == -1:
-      print "Page format is change!Can not find imag url!"
-      return "Image Url not found!"
+      print "解析标签失败，可能页面格式已经改变!"
+      return "图片URL解析失败!"
       
 def downLoadImage(url):
     if url.startswith("http"):
       picName = url[url.rfind("/") + 1:]
-      print "Downloading:" + url
+      print "准备下载:" + url
       urllib.urlretrieve(url, picName)
-      print "Saved " + picName + " in current path!"
+      print "保存 " + picName + " 在当前路径下!"
     else:
-      print url + " is not a url!"
+      print url + " 不是一个正确的图片URL地址!"
     
-downLoadImage(getImageUrl())
+if __name__ == '__main__':
+   downLoadImage(getImageUrl(readCNBingPage()))
        
    
